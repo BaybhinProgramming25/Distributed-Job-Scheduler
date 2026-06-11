b5ba371322b9cf2a2b169ed1adb8ca4f6dc0113f
Baybhin Gurung <47542952+BaybhinProgramming25@users.noreply.github.com>
2026-06-11 23:35:12 +0000

    Add base logic for the scheduler and its respective worker

---
+++ scheduler/main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        // This will be our orchestrator that will manage the worker nodes and distribute the jobs to them.
+        
+
+    }
+}
---
+++ scheduler_worker/main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // Use Javalin to create 10 instances of worker nodes 
+        
+
+    }
+}

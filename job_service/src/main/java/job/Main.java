c95c0bbb555cb0958e2d8d7c64e8770cab792cdd
Baybhin Gurung <47542952+BaybhinProgramming25@users.noreply.github.com>
2026-06-15 01:05:04 +0000

    Add placeholder login and signup endpoints

--- job_service/src/main/java/job/Main.java
+++ job_service/src/main/java/job/Main.java
@@ -5,11 +5,11 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
 
-    private static final int DEFAULT_MAX_RETRIES = 10;
+    private static final int STARTING_MAX_RETRIES = 0;
+    private static final int MAX_RETRIES_LIMIT = 10;
 
     public static void main(String[] args) {
 
@@ -17,7 +17,16 @@ public class Main {
             config.bundledPlugins.enableCors(cors -> cors.addRule(rule -> rule.anyHost()));
         }).start(7000);
 
-        // Create a new job
+        app.post("/login", ctx -> {
+            LoginRequest request = ctx.bodyAsClass(LoginRequest.class);
+            ctx.status(201).result("Logged In Successfully!");
+        });
+
+        app.post("/signup", ctx -> {
+            SignUpRequest request = ctx.bodyAsClass(SignUpRequest.class);
+            ctx.status(201).result("User Created Successfully!");
+        });
+
         app.post("/job", ctx -> {
             JobRequest request = ctx.bodyAsClass(JobRequest.class);
             Job job = insertJob(request);
@@ -25,19 +34,25 @@ public class Main {
         });
     }
 
+    public static void checkLogin() {
+        System.out.println("Pass");
+    }
+
+    public static void insertUser() {
+        // Need to implement the ogi
+    }
+
     public static Job insertJob(JobRequest request) {
         Job job = new Job(
-            request.UserId(),
-            UUID.randomUUID(),
-            request.Name(),
-            request.Type(),
-            "Scheduled",
-            request.isRecurring(),
+            request.UserId(), 
+            UUID.randomUUID(), 
             request.Schedule(),
-            0,
-            DEFAULT_MAX_RETRIES,
-            Timestamp.from(Instant.now())
+            STARTING_MAX_RETRIES, 
+            MAX_RETRIES_LIMIT, 
+            Timestamp.from(Instant.now()) 
         );
+
+        // Add it to the CockroachDB at a later time 
 
         // Create the job for now, add the database logic later on 
         System.out.println("Received job: " + job);

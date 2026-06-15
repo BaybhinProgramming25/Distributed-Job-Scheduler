b268b4e16af36007b304e06ed05677471809f23c
Baybhin Gurung <47542952+BaybhinProgramming25@users.noreply.github.com>
2026-06-15 21:14:40 +0000

    Update schema

--- job_service/schema/schema.sql
+++ job_service/schema/schema.sql
@@ -1,24 +1,16 @@
 CREATE DATABASE IF NOT EXISTS dist_jobs_scheduler;
 
-CREATE TABLE IF NOT EXISTS dist_jobs_scheduler.users(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name STRING(255) NOT NULL,
-    email STRING(255) NOT NULL,
-    password_hash STRING NOT NULL,
-)
-
 CREATE TABLE IF NOT EXISTS dist_jobs_scheduler.jobs(
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid()
     schedule STRING NOT NULL, -- CRON String
     retries_count INTEGER NOT NULL DEFAULT 0,
     max_retries INTEGER NOT NULL DEFAULT 10,
-    created_at TIMESTAMPTZ NOT NULL -- Want to get it from the backend instaed 
+    created_at TIMESTAMPTZ NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS dist_jobs_scheduler.next_jobs(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    next_job_time TIMESTAMPTZ NOT NULL, -- Next time is calculated from the backend using CRON string 
+    next_job_time TIMESTAMPTZ NOT NULL,
     job_id UUID NOT NULL REFERENCES jobs(id) ON DELETE CASCADE,
 )
 
@@ -27,5 +19,5 @@ CREATE TABLE IF NOT EXISTS dist_jobs_scheduler.execution_history(
     job_id UUID NOT NULL REFERENCES jobs(id) ON DELETE CASCADE,
     execution_time INTEGER NOT NULL,
     isCompleted BOOLEAN NOT NULL,
-    last_update_time TIMESTAMPTZ NOT NULL -- Keep track of when the worker nodes update the job when they're working on it 
+    last_update_time TIMESTAMPTZ NOT NULL
 )

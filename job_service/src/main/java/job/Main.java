5de525b383885258e1d2edf4392a78abb1dd20d8
Baybhin Gurung <47542952+BaybhinProgramming25@users.noreply.github.com>
2026-06-17 16:00:29 +0000

    Start filling in the logic for inserting in SQL

--- job_service/src/main/java/job/Main.java
+++ job_service/src/main/java/job/Main.java
@@ -2,6 +2,9 @@ package job;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
@@ -29,37 +32,72 @@ public class Main {
 
     }
 
-    // Function is void for now 
     public static Job jobSubmission(JobRequest request) {
 
         // Create the job 
         Job job = new Job(UUID.randomUUID(), request.Schedule(), STARTING_MAX_RETRIES, MAX_RETRIES_LIMIT, Timestamp.from(Instant.now()));
 
+        // String SQL
+        String sql = "INSERT INTO jobs (id, schedule, retries_count, max_retries, created_at) VALUES (?, ?, ?, ?, ?)";
+
         // Setup database connection
+        try(
+            Connection conn = Database.getDataSource().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+        ) {
+            stmt.setObject(1, job.JobId());
+            stmt.setObject(2, job.Schedule());
+            stmt.setObject(3, job.retryCount());
+            stmt.setObject(4, job.maxRetries());
+            stmt.setObject(5, job.createdAt());
+            
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted < 1) {
+                System.out.Println("Couldn't add to database!");
+            }
+        }
+        catch (SQLException e) {
+            throw new runtimeException("Failed to insert job: ", e);
+        }
+        
+        // Calculate the next time and insert it into the database 
+        Timestamp nextTime = getNextRunTime(job.createdAt());
 
-
-
-        // Connect to Cockroach DB somehow then insert the job and verify if successfully inserted 
-
-
-        // Create the job for now, add the database logic later on 
-        System.out.println("Received job: " + job);
+        // Insert the next job into the table 
 
         // Return the job
         return job;
     }
 
-    public static void insertNextScheduledJob(Job job) {
+    public static Timestamp getNextRunTime(Timestamp currentTime) {
 
-        // Insert the next scheduled job after we have inserted the current job 
 
-        System.out.println("Pass");
+        // Add the cron-utils dependency later on 
+
+    
+        // Placeholder 
+        return Timestamp.from(Instant.now());
+
     }
 
+    public static void insertNextScheduledJob(Job job, Timestmap nextRunTime) {
 
-    public static Timestamp getNextTime(String cronSchedule) {
-        
-        // Placeholder return value 
-        return Timestamp.from(Instant.now());
+        NextJob nextJob = new NextJob(UUID.randomUUID(), nextRunTime, job.JobId()); 
+
+        String sql = "INSERT INTO next_jobs (id, next_job_time, job_id) VALUES (?, ?, ?)";
+
+        try (
+            Connection conn = Database.getDataSource().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+        ) {
+            stmt.setObject(1, nextJob.nextJobId());
+            stmt.setObject(2, nextJob.nextRunTime());
+            stmt.setObject(3, nextJob.jobId());
+
+            int rowsUpdated = stmt.executeUpdate();
+            if (rowsUpdated < 1) {
+                System.out.Println("Failed to insert into Database");
+            }
+        }
     }
 }
